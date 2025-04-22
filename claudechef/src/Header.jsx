31bd2e4cc063bcf9b87ad9b7db1d75b41700af1f
Ken Kambi <kenkambi43@gmail.com>
2025-04-22 20:26:41 +0300

    Updated style.css

--- claudechef/src/Header.jsx
+++ claudechef/src/Header.jsx
@@ -1,12 +1,10 @@
-
-
-
-export default function Header (){
-    
-    return(
-        <nav className="navbar">
-            <img src="src/assets/react.svg" alt="React Logo" />
-            <span>Claude Chef</span>
-        </nav>
-    )
+export default function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <img src="src/assets/react.svg" alt="React Logo" />
+        <span>Claude Chef</span>
+      </nav>
+    </header>
+  );    
 }
