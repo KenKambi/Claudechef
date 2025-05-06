863e0061441d41fde8421686e2a7eb4d2a2ec2d5
Ken Kambi <kenkambi43@gmail.com>
2025-05-06 21:45:21 +0300

    loader

--- claudechef/src/Recipe.jsx
+++ claudechef/src/Recipe.jsx
@@ -3,10 +3,9 @@ import Generated from "./Generated";
 import { deepseekGetRecipe } from "./deepseek";
 
 export default function Recipe() {
-  const [ingredients, setIngredients] = useState([
- 
-  ]);
+  const [ingredients, setIngredients] = useState([]);
   const [generate, setGenerate] = useState("");
+  const [loading, setLoading] = useState(true)
 
   const ing = ingredients.map((ingredient) => {
     return <li key={ingredient}> {ingredient} </li>;
@@ -26,8 +25,10 @@ export default function Recipe() {
 
  async function getRecipe() {
    // setGenerate((prevGenerate) => !prevGenerate);
+    setLoading(false)
     const ans = await deepseekGetRecipe(ingredients)
     setGenerate(ans)
+    setLoading(true)
   }
 
   return (
@@ -44,7 +45,7 @@ export default function Recipe() {
         <button> &#43; Add ingredient</button>
       </form>
 
-      <div> <img src="src/Circles-menu-3.gif" alt="Loading" /> </div>
+      <div>  </div>
 
       {ingredients.length <= 3 && (
         <p> Enter a list of at least three (3) ingredients above ...</p>
@@ -65,6 +66,8 @@ export default function Recipe() {
           </button>
         </div>
       ) : undefined}
+
+      {!loading ? <img className="loading" src="src/Circles-menu-3.gif" alt="Loading..." /> : ""}
 
       {generate ?  <Generated generate = {generate} /> : ""}
     </main>
