7f2081e1a139c02aa27515bd89964c86a0b0dde8
KenKambi <122309458+KenKambi@users.noreply.github.com>
2025-06-15 19:58:50 +0300

    Update deepseek.js

--- claudechef/src/deepseek.js
+++ claudechef/src/deepseek.js
@@ -5,6 +5,7 @@ const SYSTEM_PROMPT = `I am an ai assistant tasked with the role of generating a
                     Convert it into markdown in order to be rendered on a website page. \
                     Also, don't mention that you are an AI bot`;
 
+//Deepseek API call function 
 export async function deepseekGetRecipe(ingredientsArray) {
   const ingredientsString = ingredientsArray.join(",");
   const deepseekKey = import.meta.env.VITE_AI_API_KEY;
