ebf44297213178e0ff556e287c9613350720af4f
KenKambi <122309458+KenKambi@users.noreply.github.com>
2025-06-28 21:22:49 +0300

    Update deepseek.js

--- claudechef/src/deepseek.js
+++ claudechef/src/deepseek.js
@@ -1,9 +1,3 @@
-// const SYSTEM_PROMPT = `I am an ai assistant tasked with the role of generating a recipe \
-//                     based on the list of ingredints given. \
-//                     Use them plus other suggested ingredients to generate a recipe. \
-//                     Make it interesting and use emojis. \
-//                     Convert it into markdown in order to be rendered on a website page. \
-//                     Also, don't mention that you are an AI bot`;
 
 //Deepseek API call function 
 export async function deepseekGetRecipe(ingredients) {
